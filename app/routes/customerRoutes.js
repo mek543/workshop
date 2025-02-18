156c07b2dcefb469e4a4cb3f259bdd01b5366278
mek543 <ggggg14922@gmail.com>
2025-02-19 02:09:14 +0700

    frist commit

--- app/routes/customerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const customerController = require("../controllers/customerController");
-
-router.get("/", customerController.getAllCustomers);
-router.post("/", customerController.createCustomer);
-
-module.exports = router;
